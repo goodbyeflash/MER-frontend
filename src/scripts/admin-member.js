7318cd877810e987124aadd8ec9f894b2fd20e66
yejin <kofg3137@gmail.com>
2022-08-01 23:48:04 +0900

    save

--- src/scripts/admin-member.js
+++ src/scripts/admin-member.js
@@ -1,1 +1,9 @@
 import '../styles/admin.scss';
+
+window.onload = () => {
+  var lnb = window.$('nav > ul > li');
+  lnb.on('mouseenter',function(){
+    window.$(this).siblings().removeClass("active");
+    window.$(this).addClass("active");
+  });
+};
