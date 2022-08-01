66c5416ec5636308eed8a690599df66167c61505
goodbyeFlash <goodbyeflash.dev@gmail.com>
2022-08-02 00:20:35 +0900

    DB 연결 URL 수정

--- src/scripts/form-info.js
+++ src/scripts/form-info.js
@@ -2,24 +2,9 @@ import '../styles/index.scss';
 import axios from "axios";
 
 window.onload = () => {
-    
-console.log("ASDF");
-
-    axios.get("/api/users").then((result) => {
+    axios.get(`${window.location.protocol}//${window.location.hostname}:${window.location.hostname.indexOf("goodbye-flash") > -1 ? 34000 : 4000}/api/users`).then((result) => {
         console.log(result.data);
     }).catch((error) => {
         console.error(error);
     });
-
-    // {
-    //     "name" : "김학생",
-    //     "sex" : "여",
-    //     "age" : "11세",
-    //     "address" : "서울시",
-    //     "schoolName" : "서울",
-    //     "schoolCode" : "S000001",
-    //     "type" : "중학교",
-    //     "grade" : "2학년"
-    //   }
-
 };
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -62,13 +62,13 @@ module.exports = {
       },
     ],
   },
-  devServer: {
-    port: 80, // 클라이언트 포트 번호
-    proxy: {
-      '/api/': { // /api/로 시작하는 url은 아래의 전체 도메인을 추가하고, 옵션을 적용 
-        target: 'http://localhost:4000', // 클라이언트에서 api로 보내는 요청은 주소를 4000 바꿔서 보내겠다 라는 뜻
-        changeOrigin: true, // cross origin 허용 설정
-      },
-    },
-  },
+  // devServer: {
+  //   port: 443, // 클라이언트 포트 번호
+  //   proxy: {
+  //     '/api/': { // /api/로 시작하는 url은 아래의 전체 도메인을 추가하고, 옵션을 적용 
+  //       target: 'https://localhost:4000', // 클라이언트에서 api로 보내는 요청은 주소를 4000 바꿔서 보내겠다 라는 뜻
+  //       changeOrigin: true, // cross origin 허용 설정
+  //     },
+  //   },
+  // },
 };
