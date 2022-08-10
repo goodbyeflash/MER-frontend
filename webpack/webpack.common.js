6f3e0ec5317c7c266c33588b5ae8eedeef11c298
yejin <kofg3137@gmail.com>
2022-08-10 21:39:32 +0900

    save

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,11 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-<<<<<<< HEAD
-const htmlPageNames = ['form-info','form-join','admin-member', 'admin-login','training', 'admin-member-detail', 'mer', 'mer-detail'];
-=======
-const htmlPageNames = ['form-info','form-join','admin-member','admin','training'];
->>>>>>> a38a1c585864170a5da308aed412836e86ad34d8
+const htmlPageNames = ['form-info','form-join','admin-member','training', 'admin', 'admin-member-detail', 'mer', 'mer-detail'];
 
 const multipleHtmlPlugins = htmlPageNames.map(name => {
   return new HtmlWebpackPlugin({
@@ -24,14 +20,10 @@ module.exports = {
     'form-join' : Path.resolve(__dirname, '../src/scripts/form-join.js'),
     'admin-member' : Path.resolve(__dirname, '../src/scripts/admin-member.js'),
     'training' : Path.resolve(__dirname, '../src/scripts/training.js'),
-<<<<<<< HEAD
-    'admin-login' : Path.resolve(__dirname, '../src/scripts/admin-member.js'),
     'admin-member-detail' : Path.resolve(__dirname, '../src/scripts/admin-member-detail.js'),
     'mer' : Path.resolve(__dirname, '../src/scripts/mer.js'),
     'mer-detail' : Path.resolve(__dirname, '../src/scripts/mer-detail.js'),
-=======
     'admin' : Path.resolve(__dirname, '../src/scripts/admin.js'),
->>>>>>> a38a1c585864170a5da308aed412836e86ad34d8
   },
   output: {
     path: Path.join(__dirname, '../build'),
@@ -51,7 +43,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: Path.resolve(__dirname, '../src/index.html'),
       chunks : ['main']
-    }),    
+    }),
   ].concat(multipleHtmlPlugins),
   resolve: {
     alias: {
