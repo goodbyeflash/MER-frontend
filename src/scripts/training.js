42bd4a33a1f166782d70a856cfdcd9376a10c527
goodbyeFlash <goodbyeflash.dev@gmail.com>
2022-08-15 21:55:45 +0900

    - age int로 변경 - training.js 로컬스토리지 값 없을 경우 join페이지로 이동

--- src/scripts/training.js
+++ src/scripts/training.js
@@ -1,19 +1,21 @@
 window.onload = () => {
-    
-    const onClickMERContent = (id) => {
-        document.getElementById("list").style.display = "none";
-        document.getElementById("iframe").src = `./public/content/${id}/index.html`;
-        document.getElementById("frameArea").style.display = "block";
-        //Todo.. 모바일 체크해서 가로로 변경
-        //document.getElementsByTagName("html")[0].style.transform = 'rotate(90deg)';
-    };
+  if (!window.localStorage.getItem('user_data')) {
+    location.href = 'form-info.html';
+  }
 
-    document.addEventListener('click',(e) => {
-        if( e.target.id.indexOf("MER_") > -1 ) {
-            onClickMERContent(e.target.id);
-        }
-    });
+  const onClickMERContent = (id) => {
+    document.getElementById('list').style.display = 'none';
+    document.getElementById('iframe').src = `./public/content/${id}/index.html`;
+    document.getElementById('frameArea').style.display = 'block';
+    //Todo.. 모바일 체크해서 가로로 변경
+    //document.getElementsByTagName("html")[0].style.transform = 'rotate(90deg)';
+  };
 
-    onClickMERContent("MER_01");
+  document.addEventListener('click', (e) => {
+    if (e.target.id.indexOf('MER_') > -1) {
+      onClickMERContent(e.target.id);
+    }
+  });
 
+  onClickMERContent('MER_01');
 };
